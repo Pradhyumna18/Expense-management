c72c4fcf569efa93fabc5dd2ffb535f5a99d860b
Pradhyumna18 <pradhyumna0204@gmail.com>
2020-04-07 18:59:03 +0530

    delete and edit transaction on specific account

--- src/components/Accounts/accounts.js
+++ src/components/Accounts/accounts.js
@@ -1,43 +1,42 @@
 import React from 'react'
-import { Redirect,Link } from 'react-router-dom'
-import {getTransactions} from '../../services/transactions'
+import { Redirect, Link } from 'react-router-dom'
+import { getTransactions } from '../../services/transactions'
 import TransactionDisplay from '../Transactions/transactionDisplay'
-import {getAccounts} from '../../services/accounts'
+import { getAccounts } from '../../services/accounts'
 import './accounts.css'
 class Accounts extends React.Component {
-    state={
-        onDelete:false,
-        divClicked:"",
-        }
-     componentWillMount() {
+    state = {
+        onDelete: false,
+        divClicked: "",
+    }
+    componentWillMount() {
         let accountId = localStorage.getItem("accountId");
         if (!accountId) {
             localStorage.setItem("accountId", 0)
         }
     }
-    handleDelete=()=>
-    {
-        this.setState({onDelete:true})
+    handleDelete = () => {
+        this.setState({ onDelete: true })
     }
     handleDivClicked = (name) => {
         this.setState({
-        divClicked:name,
+            divClicked: name,
         })
-        }
+    }
     render() {
-        let transactions=getTransactions()
+        let transactions = getTransactions()
         console.log(transactions)
-        let accounts=getAccounts()
+        let accounts = getAccounts()
         console.log(window.location.pathname)
         return (
             <div>
-               <div style={{ margin: "25px" }}>
+                <div style={{ margin: "25px" }}>
                     <div style={{ textAlign: "left", marginLeft: "25px" }}>
                         <label style={{ fontWeight: "bold" }} >ACCOUNTS</label>
                     </div>
                     <div style={{ overflowX: "auto", display: "flex" }}>
                         {accounts.map(obj => {
-                            return (<div className="AccountCard" onClick={()=>{this.handleDivClicked(obj.accountName)}}>
+                            return (<div className="AccountCard" onClick={() => { this.handleDivClicked(obj.accountName) }}>
                                 {obj.accountName}
                                 <b style={{ fontSize: "larger" }}> ₹ {obj.accountBalance} </b>
                             </div>)
@@ -54,9 +53,9 @@ class Accounts extends React.Component {
                     </div>
                 </div>
                 <div style={{ marginLeft: "50px" }} >
-                {transactions.map(item=>{
-                    return <TransactionDisplay onDelete={this.handleDelete}>{item}</TransactionDisplay>
-                })}
+                    {transactions.map(item => {
+                        return <TransactionDisplay onDelete={this.handleDelete}>{item}</TransactionDisplay>
+                    })}
                 </div>
                 {this.state.divClicked ? <Redirect to={`/specificAccountTransactions/${this.state.divClicked}`} /> : null}
             </div>
--- src/components/Transactions/specificAccountTransaction.js
+++ src/components/Transactions/specificAccountTransaction.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { deleteTransaction,getTransactionByAccountName } from '../../services/transactions'
 import { Redirect, Link } from 'react-router-dom'
 class TransactionDisplay extends React.Component {
-    // handleDelete = async () => {
-    //     await deleteTransaction(this.props.children.transactionId)
-    //     this.props.onDelete()
+    handleDelete = async (transactionId) => {
+        console.log(transactionId)
+        await deleteTransaction(transactionId)
+        //this.props.onDelete()
 
-    // }
+    }
     render() {
         let transactions=getTransactionByAccountName()
         return (
@@ -19,8 +20,8 @@ class TransactionDisplay extends React.Component {
                 <div> {obj.date}</div>
                 <div> {obj.amount}</div>
                 <div>{obj.accountId}</div>
-                   <button onClick={this.handleDelete}>delete transaction</button>
-                   {/* <Link to={`/edittransaction/${this.props.children.transactionId}`}>edit transaction</Link> */}
+                   <button onClick={()=>this.handleDelete(obj.transactionId)}>delete transaction</button>
+                    <Link to={`/edittransaction/${obj.transactionId}`}>edit transaction</Link> 
                </div>
             })}
            </div>
